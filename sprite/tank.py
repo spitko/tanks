138f7b8857b5d1c9dade8af45563f9f4a47b91fa
Eero Pihelgas <eero.pihelgas@gmail.com>
2018-11-15 20:15:32 +0200

    Optimize

--- sprite/tank.py
+++ sprite/tank.py
@@ -3,68 +3,92 @@ from pygame.mixer import Sound, Channel
 from pygame.sprite import Sprite, Group
 from pygame.transform import *
 
-from sprite.shell import Shell
+
+class Shell(Sprite):
+
+    image = image.load("resources/images/shell.png")
+
+    def __init__(self, tank):
+        Sprite.__init__(self)
+        self.direction = tank.direction.copy()
+        self.image = tank.shell_image
+        self.rect = self.image.get_rect()
+        self.rect.x = tank.rect.x + 4
+        self.rect.y = tank.rect.y + 4
+        self.speed = 10
+
+    def update(self):
+        for _ in range(self.speed):
+            if display.get_surface().get_rect().contains(self.rect):
+                self.rect = self.rect.move(self.direction.x, self.direction.y)
+            else:
+                self.kill()
+
 
 class Tank(Sprite):
+
     mixer.init()
     engine_sound = Sound("resources/sounds/engine.ogg")
     engine_sound_channel = Channel(1)
-    shell_sound = Sound("resources/sounds/shot.ogg")
-    shell_sound_channel = Channel(2)
+    gun_sound = Sound("resources/sounds/shot.ogg")
+    gun_sound_channel = Channel(2)
     image_sheet = image.load("resources/images/tank.png")
 
     def __init__(self, x, y):
         Sprite.__init__(self)
         self.direction = Rect(0, 0, 0, 0)
-        self.facing = 0
         self.image = Tank.image_sheet.subsurface(Rect(0, 0, 16, 16))
+        self.moving = False
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed = 5
+        self.shell_image = Shell.image
+        self.speed = 2
         self.static_image = self.image
         self.shells = Group()
 
     def move_down(self):
-        self.image = flip(self.static_image, False, True)
         self.direction.x = 0
-        self.direction.y = +self.speed
-        self.facing = 2
+        self.direction.y = 1
+        self.image = flip(self.static_image, False, True)
+        self.moving = True
+        self.shell_image = flip(Shell.image, False, True)
 
     def move_left(self):
-        self.image = flip(rotate(self.static_image, -90), True, False)
-        self.direction.x = -self.speed
+        self.direction.x = -1
         self.direction.y = 0
-        self.facing = 3
+        self.image = flip(rotate(self.static_image, -90), True, False)
+        self.moving = True
+        self.shell_image = flip(rotate(Shell.image, -90), True, False)
 
     def move_right(self):
-        self.image = rotate(self.static_image, -90)
-        self.direction.x = +self.speed
+        self.direction.x = 1
         self.direction.y = 0
-        self.facing = 1
+        self.image = rotate(self.static_image, -90)
+        self.moving = True
+        self.shell_image = rotate(Shell.image, -90)
 
     def move_up(self):
-        self.image = self.static_image
         self.direction.x = 0
-        self.direction.y = -self.speed
-        self.facing = 0
+        self.direction.y = -1
+        self.image = self.static_image.copy()
+        self.moving = True
+        self.shell_image = Shell.image.copy()
 
     def stop(self):
+        self.moving = False
         Tank.engine_sound_channel.stop()
-        self.direction.x = 0
-        self.direction.y = 0
 
     def fire(self):
-        self.shells.add(Shell(self.rect.x + 4, self.rect.y + 4, self.facing))
-        Tank.shell_sound_channel.play(Tank.shell_sound)
+        Tank.gun_sound_channel.play(Tank.gun_sound)
+        self.shells.add(Shell(self))
 
     def update(self):
         self.shells.update()
-        if self.direction.x or self.direction.y:
-            rect = self.rect.move(self.direction.x, self.direction.y)
-            if display.get_surface().get_rect().contains(rect):
-                if not Tank.engine_sound_channel.get_busy():
-                    Tank.engine_sound_channel.play(Tank.engine_sound, -1)
-                self.rect = rect
-            else:
-                self.stop()
+        if self.moving:
+            for _ in range(self.speed):
+                rect = self.rect.move(self.direction.x, self.direction.y)
+                if display.get_surface().get_rect().contains(rect):
+                    if not Tank.engine_sound_channel.get_busy():
+                        Tank.engine_sound_channel.play(Tank.engine_sound, -1)
+                    self.rect = rect
--- tanks.pyw
+++ tanks.pyw
@@ -1,47 +1,54 @@
 from pygame import *
 from pygame.sprite import Group
+from pygame.time import Clock
 from sprite.tank import Tank
 
-init()
-background = (0, 0, 0)
-display.set_caption("Tanks")
-mixer.music.load("resources/sounds/start.ogg")
-mixer.music.play()
-screen = display.set_mode((640, 480))
-player1 = Tank(312, 460)
-players = Group(player1)
-running = True
-while running:
-    for e in event.get():
-        if e.type == QUIT:
-            running = False
-        if e.type == KEYDOWN:
-            if e.key == K_ESCAPE:
+BACKGROUND_COLOR = Color("black")
+RESOLUTION = (640, 480)
+
+
+def main():
+    init()
+    display.set_caption("Tanks")
+    mixer.music.load("resources/sounds/start.ogg")
+    mixer.music.play()
+    player1 = Tank(312, 460)
+    players = Group(player1)
+    running = True
+    screen = display.set_mode(RESOLUTION)
+    while running:
+        for e in event.get():
+            if e.type == QUIT:
                 running = False
-            if e.key == K_RETURN:
-                if screen.get_flags() & FULLSCREEN:
-                    display.set_mode((640, 480))
-                else:
-                    display.set_mode((640, 480), FULLSCREEN)
-            if e.key == K_DOWN:
-                player1.move_down()
-            if e.key == K_LEFT:
-                player1.move_left()
-            if e.key == K_RIGHT:
-                player1.move_right()
-            if e.key == K_UP:
-                player1.move_up()
-            if e.key == K_SPACE:
-                player1.fire()
+            if e.type == KEYDOWN:
+                if e.key == K_ESCAPE:
+                    running = False
+                if e.key == K_RETURN:
+                    if screen.get_flags() & FULLSCREEN:
+                        display.set_mode(RESOLUTION)
+                    else:
+                        display.set_mode(RESOLUTION, FULLSCREEN)
+                if e.key == K_DOWN:
+                    player1.move_down()
+                if e.key == K_LEFT:
+                    player1.move_left()
+                if e.key == K_RIGHT:
+                    player1.move_right()
+                if e.key == K_UP:
+                    player1.move_up()
+                if e.key == K_SPACE:
+                    player1.fire()
+            if e.type == KEYUP:
+                if e.key in [K_DOWN, K_LEFT, K_RIGHT, K_UP]:
+                    player1.stop()
+        screen.fill(BACKGROUND_COLOR)
+        players.update()
+        players.draw(screen)
+        for player in players:
+            player.shells.draw(screen)
+        display.update()
+        Clock().tick(50)
 
-        if e.type == KEYUP:
-            if e.key in [K_DOWN, K_LEFT, K_RIGHT, K_UP]:
-                player1.stop()
-    screen.fill(background)
-    players.update()
-    players.draw(screen)
-    for player in players:
-        player.shells.draw(screen)
 
-    display.update()
-    time.delay(100)
+if __name__ == "__main__":
+    main()
